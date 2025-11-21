a879a6f1e986f80e0005cbda8aff8a912db6e3c6
Erthen <findmyprpt@gmail.com>
2025-11-21 11:03:26 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const path = require("path");
-require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
-app.get("/api/hello", (req, res) => {
-    res.send({ message: "AI Tutor Backend Working!" });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Server running on http://localhost:" + PORT);
+  console.log("Server running on port " + PORT);
 });
