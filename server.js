ac270f8a57aa193046f2cc603ba27c7a34d5dfa6
Erthen <findmyprpt@gmail.com>
2025-12-01 01:14:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
@@ -6,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// PUBLIC folder
+// PUBLIC folder ko serve karo
 app.use(express.static(path.join(__dirname, "public")));
 
 // Index.html load
